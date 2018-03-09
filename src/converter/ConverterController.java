8287b7937bfec30ad55455ac9cf490b9988a0b1b
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2018-03-09 14:40:44 +0700

    first commit

---
+++ src/converter/ConverterController.java
@@ -0,0 +1,15 @@
+package converter;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+/**
+ * UI controller for events and initializing components.
+ */
+public class ConverterController {
+    @FXML
+    TextField textfield1;
+    @FXML
+    TextField textfield2;
+}
